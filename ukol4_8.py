6e5f8d61fc679f6496fba16f768b6fb7ffd9ca23
Zuzana Vojtíšková <ZuzanaVo@email.cz>
2018-04-17 09:18:45 +0200

    Vlozen ukol 4, projekt 8.

---
+++ ukol4_8.py
@@ -0,0 +1,8 @@
+# --- domaci projekty 4 - ukol 8 ---
+
+pocet_radku = int(input("Zadej pocet radku: "))
+
+for cislo in range (pocet_radku):
+    mocnina = cislo**2
+    print (cislo, "na druhou je", cislo)
+
