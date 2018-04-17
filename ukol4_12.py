478832b247cb3214a5e350ff6d2eb960a74998e4
Zuzana Vojtíšková <ZuzanaVo@email.cz>
2018-04-17 09:20:08 +0200

    Vlozen ukol 4, projekt 12.

---
+++ ukol4_12.py
@@ -0,0 +1,9 @@
+# --- domaci projekty 4 - ukol 12 ---
+
+pocet_radku = int(input("Zadej pocet radku (a zaroven sloupcu): "))
+
+for cislo_radku in range(pocet_radku):
+    if (cislo_radku == 0):
+        print ("první řádek")
+    else:
+        print ("není první")
