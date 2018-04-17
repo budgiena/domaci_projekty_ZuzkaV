70607ed49b3c48fb3fbb36bac50da3abc656c35b
Zuzana Vojtíšková <ZuzanaVo@email.cz>
2018-04-17 09:20:59 +0200

    Vlozen ukol 4, projekty 14 - 18.

---
+++ ukol4_15_16_17_18.py
@@ -0,0 +1,16 @@
+# --- domaci projekty 4 - ukol 15, 16, 17, 18 ---
+# 15.
+for c in "Ahoj světe!":
+    print (c)
+# vypise jednotlive znaky z retezce
+    
+# 16.
+# pismeno prip. znak
+
+# 17.
+for c in 38:
+    print (c)
+# chyba
+
+# 18.
+# Vypise (vybere) jednotlive prvky v retezci v ramci zadaneho rozmezi (= range)
