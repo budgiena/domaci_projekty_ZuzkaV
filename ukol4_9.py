6dcf124a464611225db2b3d370b907c89ae992e3
Zuzana Vojtíšková <ZuzanaVo@email.cz>
2018-04-17 09:19:04 +0200

    Vlozen ukol 4, projekt 9.

---
+++ ukol4_9.py
@@ -0,0 +1,7 @@
+# --- domaci projekty 4 - ukol 9 ---
+pocet_radku = int(input("Zadej pocet radku (a zaroven sloupcu): "))
+for cislo in range (pocet_radku):
+    for cislo2 in range (pocet_radku):
+        print ("X ", end = "")
+    print ("")    
+        
