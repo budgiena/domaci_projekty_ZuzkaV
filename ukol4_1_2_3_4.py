36876e00b13d591cc9198aa8287a1b43e0a11421
Zuzana Vojtíšková <ZuzanaVo@email.cz>
2018-04-17 09:14:46 +0200

    Vlozen ukol 4, projekty 1-4.

---
+++ ukol4_1_2_3_4.py
@@ -0,0 +1,24 @@
+# --- Domaci projekty 4 ---
+
+# 0. Vypise promennou, retezec, ...
+
+# 1. boolean (true)??
+
+# 2. vraci zadanou hodnotu na konci vypisu (misto defaultniho vypisu noveho radku)
+
+# 3. vypise zadany delici znak mezi vypsanymi promennymi
+
+# 4. 
+##int("blablabla")
+##int("8.9")
+##ValueError: invalid literal for int() with base 10: 'blablabla' - python po nas chce v tomto prikazu cislo, ale dostal jiny datovy typ
+##
+##float("blablabla")
+##ValueError: could not convert string to float: 'blablabla' - udelali jsme stejnou chybu, jen nam to python jinak rika?
+##
+##int(8.9)
+##nevraci chybu, jen zaokrouhli na cela cisla vzdy dolu (tzn. prevadeni typu)
+
+# 5.
+for i in range(4):
+    print ("a")
