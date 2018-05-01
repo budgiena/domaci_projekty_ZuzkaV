e50035011994e8b1c078b957a77242f96f7ba97b
Zuzana Vojtíšková <ZuzanaVo@email.cz>
2018-05-01 10:58:18 +0200

    Oprava ukolu4_13.

--- ukol4_13.py
+++ ukol4_13.py
@@ -1,18 +1,27 @@
 # --- domaci projekty 4 - ukol 13 ---
 
 pocet_radku = int(input("Zadej pocet radku (a zaroven sloupcu): "))
-  
-for cislo_radku in range(pocet_radku):
-    if cislo_radku in (0,pocet_radku-1):
-        for prvni_posledni in range(pocet_radku):
-            print ("X", end = " ")
-        print ("")
-    else:    
-        for cislo_sloupce in range (pocet_radku):
-            if cislo_sloupce in (0,pocet_radku-1):
-                print ("X", end = " ")
-            else:
-                print (" ", end = " ")
-        print ("")
 
-# myslim, ze je to dost kostrbate, ale nic lepsiho me zatim nenapadlo :-)        
+"""
+# puvodni varianta 
+##for cislo_radku in range(pocet_radku):
+##    if cislo_radku in (0,pocet_radku-1):
+##        for prvni_posledni in range(pocet_radku):
+##            print ("X", end = " ")
+##        print ("")
+##    else:    
+##        for cislo_sloupce in range (pocet_radku):
+##            if cislo_sloupce in (0,pocet_radku-1):
+##                print ("X", end = " ")
+##            else:
+##                print (" ", end = " ")
+##        print ("")
+##     
+"""
+for cislo_radku in range(pocet_radku):
+    for cislo_sloupce in range(pocet_radku):
+        if (cislo_radku in (0, pocet_radku-1)) or (cislo_sloupce in (0, pocet_radku-1)):
+            print ("X", end = " ")
+        else:
+            print (" ", end = " ")
+    print ("")
